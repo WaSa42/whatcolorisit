70d35cebdb3e3bb88d2cba6cb01452e0bb7cb66e
Nicolas Rouvière <nicolasrouviere6@gmail.com>
2016-11-12 01:41:06 +0100

    All app

---
+++ src/Clock.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import { pad } from 'underscore.string';
+import './Clock.css';
+
+class Clock extends Component {
+    constructor() {
+        super();
+        moment.locale( window.navigator.userLanguage || window.navigator.language);
+        this.state = Clock.getData();
+    }
+    componentDidMount() {
+        const intervalId = setInterval(this.update.bind(this), 1000);
+        this.setState({intervalId: intervalId});
+    }
+    componentWillUnmount() {
+        clearInterval(this.state.intervalId);
+    }
+    static getData() {
+        return {
+            date: moment().format('LL'),
+            time: moment().format('LTS'),
+            hexCode: '#' + pad(moment().hours(), 2, '0') + pad(moment().minutes(), 2, '0') + pad(moment().seconds(), 2, '0'),
+        };
+    }
+    update() {
+        this.setState(Clock.getData());
+    }
+    render() {
+        return (
+            <div className="clock" style={{'backgroundColor': this.state.hexCode}}>
+                <p className="time">{this.state.time}</p>
+                <p className="date">{this.state.date}</p>
+                <p className="hex-code">{this.state.hexCode}</p>
+            </div>
+        );
+    }
+}
+
+export default Clock;
